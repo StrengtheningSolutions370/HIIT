262a8e4a6402d36218a55f81556dad24a1a3c70b
Joshua Bester <u20625741@tuks.co.za>
2022-05-04 16:34:55 +0200

    Updated employees page, updated tabs

--- src/app/employees/employees-routing.module.ts
+++ src/app/employees/employees-routing.module.ts
@@ -1,29 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeesPageModule } from './employees.module';
 
 import { EmployeesPage } from './employees.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: EmployeesPage,
+    children:[
+      {
+        path: 'employee',
+        loadChildren: () => import('./employee/employee.module').then( m => m.EmployeePageModule)
+      },
+      {
+        path: 'qualification',
+        loadChildren: () => import('./qualification/qualification.module').then( m => m.QualificationPageModule)
+      },
+      {
+        path: 'employee-type',
+        loadChildren: () => import('./employee-type/employee-type.module').then( m => m.EmployeeTypePageModule)
+      },
+      {
+        path: 'qualification-type',
+        loadChildren: () => import('./qualification-type/qualification-type.module').then( m => m.QualificationTypePageModule)
+      }
+    ]
+  },
+  {
     path: '',
-    component: EmployeesPage
-  },
-  {
-    path: 'employee',
-    loadChildren: () => import('./employee/employee.module').then( m => m.EmployeePageModule)
-  },
-  {
-    path: 'qualification',
-    loadChildren: () => import('./qualification/qualification.module').then( m => m.QualificationPageModule)
-  },
-  {
-    path: 'employee-type',
-    loadChildren: () => import('./employee-type/employee-type.module').then( m => m.EmployeeTypePageModule)
-  },
-  {
-    path: 'qualification-type',
-    loadChildren: () => import('./qualification-type/qualification-type.module').then( m => m.QualificationTypePageModule)
+    redirectTo: 'tabs',
+    pathMatch: 'full'
   }
+
 ];
 
 @NgModule({
