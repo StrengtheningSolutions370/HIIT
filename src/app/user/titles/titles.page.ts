d71047d36b729a6e462236940c57a2b3d1a91e1f
Lyne <u20690844@tuks.co.za>
2022-05-06 20:37:45 +0200

    Last changes on Venue Screen

--- src/app/user/titles/titles.page.ts
+++ src/app/user/titles/titles.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-titles',
   templateUrl: './titles.page.html',
   styleUrls: ['./titles.page.scss'],
 })
-export class TitlesPage implements OnInit {
+export class TitlesPage {
+
+  titles = [
+    {name : 'Mr.',
+     description : 'a designation for a man '},
+    {name : 'Mrs',
+     description : 'a designation for a woman who is married'},
+    {name : 'Miss.',
+    description : 'a designation for a young woman '},
+    {name : 'Ms.',
+    description : 'a designation for a woman who is unmarried'},
+    {name : 'Dr.',
+    description : 'a designation for a person who has obtained a doctorate (commonly a PhD)'},
+    {name : 'Prof.',
+    description : 'a designation for a person who is a teacher of the highest rank, or is a expert in their field'},
+  ];
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
