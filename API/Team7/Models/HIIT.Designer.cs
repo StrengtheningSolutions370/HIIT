cc980a35db9908b2509e16775963669856a32f2a
Joshua Bester <u20625741@tuks.co.za>
2022-05-06 21:46:15 +0200

    Updated API

--- API/Team7/Models/HIIT.Designer.cs
+++ API/Team7/Models/HIIT.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\shann\Desktop\New folder\Team7\Models\HIIT.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Robyn\Desktop\370 System\HIIT\API\Team7\Models\HIIT.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
