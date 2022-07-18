a0e864dd4b260ffd221b1435142ed53df2df001a
shannonlnoel <u20502746@tuks.co.za>
2022-07-18 15:59:50 +0200

    Revert "Revert "Merge branch 'joshua' of https://github.com/StrengtheningSolutions370/HIIT-Team7 into joshua""
    This reverts commit a69524c4ac905e097c07ec8205020133e9ac3660.

--- Ionic/src/app/services/authentication/auth.service.ts
+++ Ionic/src/app/services/authentication/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
 import { BehaviorSubject } from 'rxjs';
 import { appUser, appUserRegister } from 'src/app/models/appUser';
 import { GlobalService } from '../global/global.service';
@@ -16,18 +15,39 @@ export class AuthService {
   //navbar authentication with observable
   private loggedIn = new BehaviorSubject<boolean>(false);
 
+  async checkToken() {
+    var token = await this.storage.getKey('token');
+    if (token == null) {
+      this.loggedIn.next(false);
+    }
+    else {
+      var tokenObj = this.global.decodeToken(token);
+      if (tokenObj == null) {
+        this.loggedIn.next(false);
+      }
+      else if (!this.global.validateTokenData(tokenObj)) {
+        this.loggedIn.next(false);
+      }
+      else this.loggedIn.next(true);
+    }
+  }
+
   get isLoggedIn() {
+    this.checkToken();
     return this.loggedIn.asObservable();
   }
 
   navLogin() {
     this.loggedIn.next(true);
-    console.log('navLogin called');
   }
 
   navLogout() {
     this.loggedIn.next(false);
-    console.log('navLogout called');
+  }
+
+  async getState() {
+    await this.checkToken();
+    return this.loggedIn.value;
   }
   //////////////
 
@@ -35,8 +55,7 @@ export class AuthService {
     private repo: RepoService,
     private global: GlobalService,
     private storage: StoreService,
-    private router: Router,
-    private cookie: CookieService) { }
+    private router: Router) { }
 
   register(registerUser: appUserRegister) {
     this.repo.register(registerUser).subscribe(result => {
@@ -45,17 +64,19 @@ export class AuthService {
   }
 
   async login(appUser: appUser) {
-   await this.global.nativeLoad('loading...');
-    return this.repo.login(appUser).subscribe((result: any) => {
+
+  await this.storage.deleteKey('token');
+
+   await this.global.nativeLoad("loading...");
+    return this.repo.login(appUser).subscribe((result : any) => { 
       var token = result.value.token;
       var expiration = result.value.expiration;
       var date = new Date(expiration);
       var epoch = date.getTime(); //convert TZ string to epoch
-
-      this.cookie.set('token', token, epoch);
+      this.storage.setKey('token', token);
       this.navLogin(); //change observable to show navbar
-      this.router.navigate(['home']);
-   }).add(() =>{this.global.endNativeLoad();});
+      this.router.navigate(['home']);   
+   }).add(() =>{this.global.endNativeLoad()});
   }
 
   async logout() {
@@ -64,8 +85,8 @@ export class AuthService {
     //   this.router.navigateByUrl('/login');
     //   this.global.endNativeLoad();
     // })
-    this.cookie.deleteAll(); //removes all cookies from client
     this.navLogout();
+    this.storage.deleteKey('token');
     this.router.navigate(['login']); //route user back to login
    }
 
