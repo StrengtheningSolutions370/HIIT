6ebe75727baac004a5279b31f2379d1f5121d0d4
shannonlnoel <u20502746@tuks.co.za>
2022-07-18 15:54:24 +0200

    Revert "Delete sale-item.ts"
    This reverts commit 3274d863d8377023497318e07bccc742380b42dc.

---
+++ Ionic/src/app/models/sale-item.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export class SaleItem {
+    SaleItemID: number;
+    Name: string;
+    Photo: number;
+    Description: string;
+    Price: number;
+    Quotable: boolean;
+    Quantity: number;
+    SaleCategoryID: number;
+}
