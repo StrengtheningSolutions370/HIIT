81a6bf3c3bea5fb6d693b602e6deb45c0790300c
Shannon Noel <u20502746@tuks.co.za>
2022-07-21 01:50:55 +0200

    Delete employee modal

--- Ionic/src/app/pages/employee/employee-page/delete-employee/delete-employee.component.ts
+++ Ionic/src/app/pages/employee/employee-page/delete-employee/delete-employee.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ModalController, ToastController, ViewWillEnter, AlertController } from '@ionic/angular';
+import { Employee } from 'src/app/models/employee';
+import { EmployeeService } from 'src/app/services/employee/employee.service';
+
 
 @Component({
   selector: 'app-delete-employee',
@@ -8,12 +13,75 @@ import { ModalController } from '@ionic/angular';
 })
 export class DeleteEmployeeComponent implements OnInit {
 
-  constructor(private modalCtrl: ModalController) { }
+  @Input() employee: Employee;
 
-  ngOnInit() {}
+  title! : string;
+  employeeType! : string;
+  qualification! : string;
 
-  dismissModal() {
-    this.modalCtrl.dismiss();
-  }
+  imgSrc = '';
+  pdfSrc = '';
+  showProfile = false;
+
+
+  constructor(private modalCtrl: ModalController, private toastCtrl: ToastController, public formBuilder: FormBuilder,
+    public employeeService: EmployeeService, private router: Router, private route: ActivatedRoute, private alertCtrl: AlertController) { }
+
+
+  ngOnInit() {
+
+      this.title = this.employee.TitleID.split(',')[1];
+      this.employeeType = this.employee.EmployeeTypeID.split(',')[1];
+      this.qualification = this.employee.QualificationID.split(',')[1];
+
+      //decode the contract
+      let reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.pdfSrc = e.target.result;
+      };
+      reader.readAsArrayBuffer(this.employee.Contract);
+
+      //attempt to decode the image
+      if (this.employee.Photo != null) {
+        this.showProfile = true;
+        const reader = new FileReader();
+        reader.onload = (event : any) => {
+          this.imgSrc = event.target.result;
+          this.showProfile = true;
+        }
+        reader.readAsDataURL(this.employee.Photo);
+      }
+
+    }
+
+     //Send through the id of the selected employee to be deleted in the employee service.
+     async delete(id: number){
+      this.employeeService.deleteEmployee(id);
+      await this.dismissModal();
+      this.sucDelete();
+    }
+  
+    async sucDelete() {
+      const toast = await this.toastCtrl.create({
+        message: 'The Employee has been successfully deleted!',
+        duration: 2000
+      });
+      toast.present();
+    }
+  
+    async failureAlert() {
+      const alert = await this.alertCtrl.create({
+        header: 'Could not delete Employee',
+        message: 'There was an error deleting the employee, please try again.',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+  
+    dismissModal() {
+      this.modalCtrl.dismiss();
+    };
 
 }
+
+
