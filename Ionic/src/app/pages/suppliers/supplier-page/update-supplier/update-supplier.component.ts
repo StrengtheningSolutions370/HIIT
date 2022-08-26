e04fda042fce5aad70429432f0e5b2054fcc8587
shannonlnoel <u20502746@tuks.co.za>
2022-08-26 17:04:58 +0200

    update to migrations for supplier

---
+++ Ionic/src/app/pages/suppliers/supplier-page/update-supplier/update-supplier.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-supplier',
+  templateUrl: './update-supplier.component.html',
+  styleUrls: ['./update-supplier.component.scss'],
+})
+export class UpdateSupplierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
