c666649a83ce4ba39917f72c72158c6fe893e40d
GeorgeLiatos <u20453061@tuks.co.za>
2022-08-27 21:35:19 +0200

    Start work on writeoff

---
+++ Core API/Team7/ViewModels/WriteOffLineViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Team7.Models;
+
+namespace Team7.ViewModels
+{
+    public class WriteOffLineViewModel
+    {
+        public WriteOff WriteOff { get; set; }
+        public int Quantity { get; set; }
+        public SaleItem[] SaleItems { get; set; }
+    }
+}
