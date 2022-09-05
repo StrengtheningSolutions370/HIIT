0cb45ef37eaf710d4447d27bc693b5f5fd9f1722
shannonlnoel <u20502746@tuks.co.za>
2022-09-05 03:57:23 +0200

    API deployed

--- Core API/Team7/Startup.cs
+++ Core API/Team7/Startup.cs
@@ -130,8 +130,7 @@ namespace Team7
 
             //DB configuration
             services.AddDbContext<AppDB>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("AWS")));
-
+            options.UseSqlServer(Configuration.GetConnectionString("Azure")));
             //Scoping all Interfaces to all Repos
             services.AddScoped<IBookingAttendanceRepo, BookingAttendanceRepo>();
             services.AddScoped<IBookingPriceHistoryRepo, BookingPriceHistoryRepo>();
