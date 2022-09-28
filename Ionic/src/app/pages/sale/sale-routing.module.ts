7c3a3e00385db771fb3ccb13d9230a6e435b4cae
GeorgeLiatos <u20453061@tuks.co.za>
2022-09-28 23:13:09 +0200

    I am confused

--- Ionic/src/app/pages/sale/sale-routing.module.ts
+++ Ionic/src/app/pages/sale/sale-routing.module.ts
@@ -34,22 +34,6 @@ const routes: Routes = [
       }
     ]
   },
-  {
-    path: '',
-    redirectTo: 'tabs/sale-item',
-    pathMatch: 'full'
-  },
-  {
-    path: 'write-off',
-    loadChildren: () => import('./write-off/write-off.module').then( m => m.WriteOffPageModule)
-  },
-  {
-    path: 'write-off-reason',
-    loadChildren: () => import('./write-off-reason/write-off-reason.module').then( m => m.WriteOffReasonPageModule)
-  }
-
-
-
 ];
 
 @NgModule({
