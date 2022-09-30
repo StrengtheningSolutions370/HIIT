aaa2f98ae85c665597a954c25f4b2313f3b2023e
shannonlnoel <u20502746@tuks.co.za>
2022-09-30 10:31:14 +0200

    migration change

--- Core API/Team7/Migrations/20220930080859_initial.Designer.cs
+++ Core API/Team7/Migrations/20220930080859_initial.Designer.cs
@@ -12,8 +12,8 @@ using Team7.Context;
 namespace Team7.Migrations
 {
     [DbContext(typeof(AppDB))]
-    [Migration("20220923110121_Initial")]
-    partial class Initial
+    [Migration("20220930080859_initial")]
+    partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
